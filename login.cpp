78467f7c8b2e0726bc88eed509ad3271d9c10714
chepiga1995 <ura1995ura@gmail.com>
2016-05-21 10:58:06 +0300

    remove password from person table; auth mathod move to db users

--- login.cpp
+++ login.cpp
@@ -12,12 +12,7 @@ Login::Login(QWidget *parent) :
     ui(new Ui::Login)
 {
     ui->setupUi(this);
-    this->tempDB = QSqlDatabase::addDatabase("QPSQL", "tempConnection");
-    this->tempDB.setHostName("52.31.46.192");
-    this->tempDB.setDatabaseName("db_project");
-    this->tempDB.setUserName("admin");
-    this->tempDB.setPassword("1230");
-    this->init();
+    this->initSuccess();
 }
 
 Login::~Login()
@@ -25,35 +20,9 @@ Login::~Login()
     delete ui;
 }
 
-void Login::init()
-{
-    bool ok = this->tempDB.open();
-    if(ok != true)
-    {
-        this->initFaild();
-    }
-    else
-    {
-        this->initSuccess();
-    }
-}
-
-void Login::initFaild()
-{
-    ui->label_3->show();
-    ui->pushButton_2->show();
-    ui->label->hide();
-    ui->label_2->hide();
-    ui->label_4->hide();
-    ui->lineEdit->hide();
-    ui->lineEdit_2->hide();
-    ui->pushButton->hide();
-}
 
 void Login::initSuccess()
 {
-    ui->label_3->hide();
-    ui->pushButton_2->hide();
     ui->label->show();
     ui->label_2->show();
     ui->label_4->hide();
@@ -67,41 +36,15 @@ void Login::on_pushButton_clicked()
     ui->label_4->hide();
     QString login = ui->lineEdit->text();
     QString password = ui->lineEdit_2->text();
-    QSqlQuery query(this->tempDB);
-    query.prepare("SELECT * FROM persons WHERE login=:login and password=:password "
-                  "and (access_type='manager' or access_type='hr');");
-    query.bindValue(":login", login);
-    query.bindValue(":password", password);
-    query.exec();
-    if(!query.isActive() || query.size() != 1)
-    {
-        ui->label_4->show();
-    }
-    else
-    {
-        query.first();
-        mainUser = query;
-        this->setupMainConnection();
-    }
-}
-
-void Login::on_pushButton_2_clicked()
-{
-    this->init();
-}
-
-void Login::setupMainConnection()
-{
     QSqlDatabase mainConnection = QSqlDatabase::addDatabase("QPSQL");
     mainConnection.setHostName("52.31.46.192");
     mainConnection.setDatabaseName("db_project");
-    mainConnection.setUserName("admin");
-    mainConnection.setPassword("1230");
-
+    mainConnection.setUserName(login);
+    mainConnection.setPassword(password);
     bool ok = mainConnection.open();
     if(ok != true)
     {
-        this->initFaild();
+        ui->label_4->show();
     }
     else
     {
@@ -111,3 +54,6 @@ void Login::setupMainConnection()
         this->hide();
     }
 }
+
+
+
--- login.h
+++ login.h
@@ -15,15 +15,10 @@ public:
     explicit Login(QWidget *parent = 0);
     ~Login();
 private:
-    void init();
-    void initFaild();
     void initSuccess();
-    void setupMainConnection();
-    QSqlDatabase tempDB;
 private slots:
     void on_pushButton_clicked();
 
-    void on_pushButton_2_clicked();
 
 private:
     Ui::Login *ui;
